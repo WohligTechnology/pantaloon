b02a56d9f8f886493d8925cd02496ddc46d69136
Chintan <chintan@wohlig.com>
2016-11-14 16:42:47 +0530

    deno

--- api/controllers/UserDetailController.js
+++ api/controllers/UserDetailController.js
@@ -1,9 +1,7 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
 var controller = {
-
   generateExcel: function (req, res) {
     UserDetail.generateExcel(res);
-  },
-
+  }
 };
 module.exports = _.assign(module.exports, controller);
