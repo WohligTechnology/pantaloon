f0917133aef5c791b616af07671860f0a80a3498
Chintan <chintan@wohlig.com>
2016-11-15 13:23:59 +0530

    pantaloon

--- config/passport/google.js
+++ config/passport/google.js
@@ -1,6 +1,6 @@
 global["GoogleKey"] = "AIzaSyATpxYDbEGF_Lig9U8KYWv9pLuxW0dU9lw";
-global["GoogleclientId"] = "303439552643-no8tbln6he1q9nka7idmtij4i341lu47.apps.googleusercontent.com";
-global["GoogleclientSecret"] = "zBUHF5vz0wvIBVq2yenqzruV";
+global["GoogleclientId"] = "980997541474-nlrvr48lhntod885dvk1kmkuo65unnes.apps.googleusercontent.com";
+global["GoogleclientSecret"] = "8LdOPIGYWiq25L3fUgkJmtA9";
 
 passport.use(new GoogleStrategy({
         clientId: GoogleclientId,
